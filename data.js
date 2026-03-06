99ad0e52dd0dbc2e8e417a03b17aca1a0b6083a6
AndreFormi <113343206+AndreFormi@users.noreply.github.com>
2026-03-06 12:53:44 +0100

    Create data.js

---
+++ data.js
@@ -0,0 +1,102 @@
+const RECIPES_DATA = [
+  {
+    title: "Klassischer Haferbrei mit Beeren",
+    ingredients: [
+      { name: "Haferflocken", qty: 100, unit: "g" },
+      { name: "Milch oder pflanzliche Alternative", qty: 300, unit: "ml" },
+      { name: "Honig", qty: 1, unit: "TL" },
+      { name: "Gemischte Beeren", qty: 100, unit: "g" },
+      { name: "Zimt", qty: 1, unit: "Prise" }
+    ]
+  },
+  {
+    title: "Apfel-Zimt-Haferflocken",
+    ingredients: [
+      { name: "Haferflocken", qty: 100, unit: "g" },
+      { name: "Wasser oder Milch", qty: 300, unit: "ml" },
+      { name: "Apfel", qty: 1, unit: "Stk" },
+      { name: "Zimt", qty: 1, unit: "TL" },
+      { name: "Ahornsirup", qty: 1, unit: "TL" }
+    ]
+  },
+  {
+    title: "Schoko-Bananen-Haferflocken",
+    ingredients: [
+      { name: "Haferflocken", qty: 100, unit: "g" },
+      { name: "Milch oder pflanzliche Alternative", qty: 300, unit: "ml" },
+      { name: "Banane", qty: 1, unit: "Stk" },
+      { name: "Kakaopulver", qty: 1, unit: "EL" },
+      { name: "Honig", qty: 1, unit: "TL" }
+    ]
+  },
+  {
+    title: "Bircher Müsli",
+    ingredients: [
+      { name: "Haferflocken", qty: 100, unit: "g" },
+      { name: "Apfelsaft", qty: 200, unit: "ml" },
+      { name: "Apfel", qty: 1, unit: "Stk" },
+      { name: "Naturjoghurt", qty: 100, unit: "g" },
+      { name: "Honig", qty: 1, unit: "TL" }
+    ]
+  },
+  {
+    title: "Übernacht-Hafer mit Chiasamen",
+    ingredients: [
+      { name: "Haferflocken", qty: 100, unit: "g" },
+      { name: "Mandelmilch", qty: 200, unit: "ml" },
+      { name: "Chiasamen", qty: 1, unit: "EL" },
+      { name: "Vanilleextrakt", qty: 1, unit: "TL" },
+      { name: "Ahornsirup", qty: 1, unit: "EL" }
+    ]
+  },
+  {
+    title: "Haferflocken-Pancakes",
+    ingredients: [
+      { name: "Haferflocken", qty: 100, unit: "g" },
+      { name: "Ei", qty: 1, unit: "Stk" },
+      { name: "Milch", qty: 100, unit: "ml" },
+      { name: "Backpulver", qty: 1, unit: "TL" },
+      { name: "Banane", qty: 1, unit: "Stk" }
+    ]
+  },
+  {
+    title: "Haferflocken-Smoothie-Bowl",
+    ingredients: [
+      { name: "Haferflocken", qty: 50, unit: "g" },
+      { name: "Banane", qty: 1, unit: "Stk" },
+      { name: "Gefrorene Beeren", qty: 200, unit: "g" },
+      { name: "Mandelmilch", qty: 200, unit: "ml" },
+      { name: "Chiasamen", qty: 1, unit: "EL" }
+    ]
+  },
+  {
+    title: "Mango-Kokos-Haferflocken",
+    ingredients: [
+      { name: "Haferflocken", qty: 100, unit: "g" },
+      { name: "Kokosmilch", qty: 300, unit: "ml" },
+      { name: "Mango", qty: 1, unit: "Stk" },
+      { name: "Kokosraspeln", qty: 1, unit: "EL" },
+      { name: "Agavendicksaft", qty: 1, unit: "TL" }
+    ]
+  },
+  {
+    title: "Protein-Haferflocken mit Erdnussbutter",
+    ingredients: [
+      { name: "Haferflocken", qty: 100, unit: "g" },
+      { name: "Milch oder pflanzliche Alternative", qty: 300, unit: "ml" },
+      { name: "Erdnussbutter", qty: 1, unit: "EL" },
+      { name: "Kakaopulver", qty: 1, unit: "TL" },
+      { name: "Honig", qty: 1, unit: "TL" }
+    ]
+  },
+  {
+    title: "Karottenkuchen-Haferflocken",
+    ingredients: [
+      { name: "Haferflocken", qty: 100, unit: "g" },
+      { name: "Milch oder pflanzliche Alternative", qty: 300, unit: "ml" },
+      { name: "Karotte", qty: 1, unit: "Stk" },
+      { name: "Zimt", qty: 1, unit: "TL" },
+      { name: "Walnüsse", qty: 1, unit: "EL" }
+    ]
+  }
+];
